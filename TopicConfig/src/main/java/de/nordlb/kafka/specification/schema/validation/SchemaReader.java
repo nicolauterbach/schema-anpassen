6ed8b1fefbea78572236a5e80f6880d45386b925
N1131854 <nico.lauterbach@nordlb.de>
2025-02-27 15:48:11 +0100

    test datei, template, workflow

--- TopicConfig/src/main/java/de/nordlb/kafka/specification/schema/validation/SchemaReader.java
+++ TopicConfig/src/main/java/de/nordlb/kafka/specification/schema/validation/SchemaReader.java
@@ -6,8 +6,8 @@ import java.nio.file.Paths;
 public class SchemaReader {
     public static void main(String[] args) {
         try {
-            // Ohne "TopicConfig/" da der Working Directory bereits TopicConfig ist
-            String schemaContent = Files.readString(Paths.get("uploaded_schema.avsc"));
+            // Lese den Inhalt der Datei "uploaded_schema.txt" als String
+            String schemaContent = Files.readString(Paths.get("uploaded_schema.txt"));
             System.out.println("Ausgelesenes Schema:");
             System.out.println(schemaContent);
         } catch (Exception e) {
